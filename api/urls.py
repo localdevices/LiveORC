de25390d146adace685fc362beedea833d6cb4f9
hcwinsemius <h.c.winsemius@gmail.com>
2023-09-08 17:03:38 +0200

    debugged api docs schema urls #9

--- api/urls.py
+++ api/urls.py
@@ -26,8 +26,5 @@ urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', include(router.urls)),
-    # path('schema/', SpectacularAPIView.as_view(), name='schema'),
-    # path('schema/docs_swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    # path('schema/docs/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 
 ]
