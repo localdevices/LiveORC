42e222c873e632e27325673ea819db5d020e5e9d
Nii <nii.nmarley@gmail.com>
2023-10-23 20:19:06 +0000

    Switch institute view #43

--- api/views/__init__.py
+++ api/views/__init__.py
@@ -1,3 +1,6 @@
+from django.conf import settings
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
 from .base import BaseModelViewSet
 from .site import SiteViewSet
 from .camera_config import CameraConfigViewSet
@@ -6,3 +9,13 @@ from .recipe import RecipeViewSet
 from .task import TaskViewSet
 from .time_series import TimeSeriesViewSet
 from .video import VideoViewSet, VideoSiteViewSet
+from users.models.institute import Institute
+
+
+@login_required()
+def switch_institute(request, institute_id):
+    institute_obj = Institute.objects.get(pk=institute_id)
+    if request.user.is_institute_member(institute_obj):
+        request.session[settings.INSTITUTE_SESSION_KEY] = institute_obj.pk
+    url_path = request.GET.get("next")
+    return redirect(url_path)
