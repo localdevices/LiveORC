50ccbb0732266d169c1a1423e0e55233250a75f0
Nii <nii.nmarley@gmail.com>
2023-10-24 15:12:25 +0000

    Add institute to API save #43

--- api/views/base.py
+++ api/views/base.py
@@ -17,8 +17,12 @@ class BaseModelViewSet(viewsets.ModelViewSet):
         site_obj = serializer.validated_data.get('site')
         camera_config_obj = serializer.validated_data.get('camera_config')
         if model_name == "CameraConfig" and site_obj:
-            serializer.save(creator=site_obj.creator)
+            institute = site_obj.institute
+            creator = site_obj.creator
         elif model_name == "Video" and camera_config_obj:
-            serializer.save(creater=camera_config_obj.site.creator)
+            institute = camera_config_obj.site.institute
+            creator = camera_config_obj.site.creator
         else:
-            serializer.save(creator=self.request.user)
+            institute = self.request.user.get_active_institute(self.request)
+            creator = self.request.user
+        serializer.save(creator=creator, institute=institute)
