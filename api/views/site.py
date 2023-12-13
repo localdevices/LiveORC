9c9dea5078c18cc001d3b3b6eb92d21ba55ace50
Nii <nii.nmarley@gmail.com>
2023-12-13 17:09:03 +0000

    Override queryset and add filter for institute

--- api/views/site.py
+++ api/views/site.py
@@ -1,6 +1,8 @@
 from api.serializers import SiteSerializer
 from api.models import Site
 from api.views import BaseModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
+
 
 class SiteViewSet(BaseModelViewSet):
 
@@ -11,3 +13,20 @@ class SiteViewSet(BaseModelViewSet):
     serializer_class = SiteSerializer
 
     http_method_names = ["post", "get", "patch", "delete"]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['institute']
+
+    def get_queryset(self):
+        """
+        Optionally restricts the returned sites to a given institute,
+        by filtering against an `institute` query parameter in the URL.
+        """
+        queryset = Site.objects.all()
+        institute = self.request.query_params.get('institute', None)
+        if self.request.user.is_superuser:
+            return queryset
+        if institute:
+            queryset = queryset.filter(institute=institute)
+        return queryset.none()
+
+
