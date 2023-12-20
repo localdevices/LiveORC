41aaf4cbffc64af8cd06c135dc73e00d08f5e3cf
Nii <nii.nmarley@gmail.com>
2023-12-20 16:18:35 +0000

    Re-setup test db #43

--- api/tests/test_setup_db.py
+++ api/tests/test_setup_db.py
@@ -1,14 +1,13 @@
 # from django.contrib.auth.models import User
-from users.models import User, Institute, Group
+from users.models import User, Institute, Group, Member
 from users.management.commands.creategroups import add_group_permissions, READ_GROUPS, WRITE_GROUPS, MODELS, READ_PERMISSIONS, WRITE_PERMISSIONS
-
-
-
 from django.test import TestCase
+
+
 def setupUsers():
     # make the default groups (as if running manage.py creategroups)
-    add_group_permissions(READ_GROUPS, MODELS, READ_PERMISSIONS)
-    add_group_permissions(WRITE_GROUPS, MODELS, WRITE_PERMISSIONS)
+    # add_group_permissions(READ_GROUPS, MODELS, READ_PERMISSIONS)
+    # add_group_permissions(WRITE_GROUPS, MODELS, WRITE_PERMISSIONS)
 
     # make some Users
     User.objects.create_superuser(name='testsuperuser',
@@ -17,28 +16,26 @@ def setupUsers():
     testuser1 = User.objects.create_user(name='testuser',
                              email='user@institute1.com',
                              password='test1234')
-    testuser1.groups.add(2)
+    # testuser1.groups.add(2)
     testuser2 = User.objects.create_user(name='testuser2',
                              email='user2@institute1.com',
                              password='test1234')
-    testuser2.groups.add(1)
+    # testuser2.groups.add(1)
     testuser3 = User.objects.create_user(name='testuser3',
                              email='user3@institute2.com',
                              password='test1234')
-    testuser3.groups.add(2)
+    # testuser3.groups.add(2)
     # make some institutes
     institute1 = Institute.objects.create(name="institute1", owner=testuser1)
     institute2 = Institute.objects.create(name="institute2", owner=testuser3)
     # now update the users with the institute
-    testuser1.institute = institute1
-    testuser2.institute = institute1
     # user 3 belong to another institute, so that we can test if he/she is denied access to instances of test users
     # of institute1
-    testuser3.institute = institute2
-    testuser1.save()
-    testuser2.save()
-    testuser3.save()
+    institute2.add_member(testuser3, "Member")
 
+    # testuser1.save()
+    # testuser2.save()
+    # testuser3.save()
 
 
 def cleanup():
@@ -46,6 +43,7 @@ def cleanup():
     Clean up any temporary file storage
     """
     print("Clean up S3 storage before test. Not implemented yet.")
+
 
 class InitTestCase(TestCase):
     '''
