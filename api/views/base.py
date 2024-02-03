dd2835dea43bbc5252152321e5bf1fcd0fa496c1
hcwinsemius <h.c.winsemius@gmail.com>
2024-02-03 13:27:42 +0100

    fix #51

--- api/views/base.py
+++ api/views/base.py
@@ -2,14 +2,28 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from api.permissions import IsOwnerOrReadOnlyAsInstitute
+from rest_framework import status
+from rest_framework.response import Response
+
+from api.models import Site
 
 
 class BaseModelViewSet(viewsets.ModelViewSet):
     """
-    This BaseModelViewSet is for all models and model instancesin the API. These all have a 'creator' as non-editable
+    This BaseModelViewSet is for all models and model instances in the API. These all have a 'creator' as non-editable
     field and should be visible only to people within the same institute or to the user that created them
     """
     permission_classes = [IsOwnerOrReadOnlyAsInstitute, IsAuthenticated]
+
+    def list(self, request, *args, **kwargs):
+        if "site_pk" in self.kwargs:
+            if not(request.user.is_institute_member(Site.objects.get(pk=self.kwargs["site_pk"]).institute) or self.request.user.is_superuser):
+                return Response(
+                {"detail": "You do not have permission to perform this action"},
+                    status=status.HTTP_403_FORBIDDEN
+                )
+        return super().list(request, *args, **kwargs)
+
 
     def perform_create(self, serializer):
         # only add the creator upon saving
