dc834fad91e0c95ffd8a39f508091c266a8191ae
hcwinsemius <h.c.winsemius@gmail.com>
2024-02-08 19:43:10 +0100

    task_form action in camera_config view #16

--- api/views/camera_config.py
+++ api/views/camera_config.py
@@ -1,5 +1,7 @@
-from rest_framework import status
+from rest_framework import status, renderers
 from rest_framework.response import Response
+from rest_framework.decorators import action
+
 from api.serializers import CameraConfigSerializer, CameraConfigCreateSerializer
 from api.models import CameraConfig
 from api.views import BaseModelViewSet
@@ -33,4 +35,12 @@ class CameraConfigViewSet(BaseModelViewSet):
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+
+    @action(detail=True, methods=['post'], renderer_classes=[renderers.StaticHTMLRenderer])
+    def create_task(self, request, device_id, *args, **kwargs):
+        instance = self.get_object()
+        # task = get_task_form(instance, request, *args, **kwargs)
+        # print(f"URL: {request.build_absolute_uri(reverse('video'))}")
+        return Response(instance.data, status=status.HTTP_200_OK)
 
