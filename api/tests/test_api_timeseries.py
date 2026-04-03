cd371426309280ec774d4183d81912ba6aff4f48
hcwinsemius <h.c.winsemius@gmail.com>
2026-04-03 12:18:49 +0200

    fixed wrong timestamp format in timeseries test #118

--- api/tests/test_api_timeseries.py
+++ api/tests/test_api_timeseries.py
@@ -95,13 +95,13 @@ class TimeSeriesViewTests(InitTestCase):
         uri = reverse("api:site-timeseries-list", args=(["1"]))
         # query in PIJSON format
         r = client.get(
-            uri + "?startDateTime=2000-01-01T04:00:00:00Z" + "&endDateTime=2000-01-01T07:00:00:00Z" + "&format=pijson"
+            uri + "?startDateTime=2000-01-01T04:00:00Z" + "&endDateTime=2000-01-01T07:00:00Z" + "&format=pijson"
         )
         self.assertEqual(len(r.json()), 3)  # test for number of fields in pijson (always 3)
         self.assertEqual(len(r.json()["timeSeries"]), 12)  # test for number of variables (currently 9)
         self.assertEqual(len(r.json()["timeSeries"][0]["events"]), 4)  # test for number of records
         # query in csv format
         r = client.get(
-            uri + "?startDateTime=2000-01-01T04:00:00:00Z" + "&endDateTime=2000-01-01T07:00:00:00Z" + "&format=csv"
+            uri + "?startDateTime=2000-01-01T04:00:00Z" + "&endDateTime=2000-01-01T07:00:00Z" + "&format=csv"
         )
         self.assertEqual(len(r.content), 372)
